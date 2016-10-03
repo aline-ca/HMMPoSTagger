b25b9bb92b99ca3f783757d7fadfc349deb4a090
Talai <aleia@mail.de>
2016-10-03 22:38:59 +0200

    Fixed formatting in HMMGenerator class. Adjusted the implementation of forward probabilities: Now the forward function is called for every single sentence in the test file seperately and a vector of double values is returned.

--- src/compute_forward_prob.cpp
+++ src/compute_forward_prob.cpp
@@ -1,5 +1,5 @@
 /* 
- * File:                        compute_viterbi.cpp
+ * File:                        compute_forward_prob.cpp
  * Author:                      Aline Castendiek
  * Student ID:                  768297
  * Date:                        30/10/15
@@ -11,11 +11,22 @@
  */
 
 
-#ifndef __COMPUTE_VITERBI_CPP__
-#define __COMPUTE_VITERBI_CPP__
+#ifndef __COMPUTE_FORWARD_PROB_CPP__
+#define __COMPUTE_FORWARD_PROB_CPP__
 
 #include <iostream>
 #include "../include/HMM.hpp"
+
+typedef std::vector<double> DoubleVector; ///< Vector of double values
+
+void print_vector_elements(DoubleVector elements)
+{
+   std::cout << "[ ";
+   for (auto it = elements.begin(); it != elements.end(); it++) {
+      std::cout << *it << ", ";
+   }
+   std::cout << " ]";
+}
 
 int main(int argc, char* argv[])
 {
@@ -27,17 +38,16 @@ int main(int argc, char* argv[])
 
       /// Set debugging parameter to true to enable debugging.
       // HMM hmm(argv[1], true);
+      DoubleVector forward_probs = hmm.get_forward_probs(argv[2]);
 
-      // Computes most likely sequence of hidden states for observation sequence 
-      // read in from file in command line
-      hmm.get_most_likely_seq(argv[2]);
+      print_vector_elements(forward_probs);
 
    }
    else {
       std::cerr <<
-              "USAGE: compute_viterbi hmm_file test_file\n\n"
+              "USAGE: compute_forward_prob hmm_file test_file\n\n"
               "hmm_file: File that contains the HMM. An example HMM file is included in /examples.\n"
-              "test_file: File that contains test sentences for which the most likely tag sequence shall be computed.\n\n";
+              "test_file: File that contains test sentences for which the forward probabilities shall be computed.\n\n";
       exit(1);
    }
    return 0;
--- src/generate_hmm.cpp
+++ src/generate_hmm.cpp
@@ -17,32 +17,31 @@
 #include <iostream>
 #include "../include/HMMGenerator.hpp"
 
-
 int main (int argc, char* argv[])
 {
-	if (argc == 3) {
+   if (argc == 3) {
 
-    // HMM generator instance that will compute probabilities without smoothing
-    // HMMGenerator hmm_generator(argv[1],argv[2],false);
+      // HMM generator instance that will compute probabilities without smoothing
+      // HMMGenerator hmm_generator(argv[1],argv[2],false);
 
-    // HMM generator instance that will perform standard add-one smoothing
-    HMMGenerator hmm_generator(argv[1],argv[2]);
+      // HMM generator instance that will perform standard add-one smoothing
+      HMMGenerator hmm_generator(argv[1], argv[2]);
 
-    // HMM generator instance that will perform smoothing with own specified summand
-    // HMMGenerator hmm_generator(argv[1],argv[2],true,0.75);
-	}
+      // HMM generator instance that will perform smoothing with own specified summand
+      // HMMGenerator hmm_generator(argv[1],argv[2],true,0.75);
+   }
 
-	else {
-		std::cerr << 
-      "USAGE: generate_hmm.cpp training_file output_file \n"
-      "The training file has to contain one word and its corresponding pos tag "
-      "per line, seperated by tabulators or spaces. Each beginning and end of a "
-      "sentence in the data must be marked with an additional <BOS> <BOS> and " 
-      "respectively <EOS> <EOS> pair also seperated by tabulators. \n"
-      "Some example files are included in /data. \n";
-    exit(1);
-	}
-	return 0;
+   else {
+      std::cerr <<
+              "USAGE: generate_hmm.cpp training_file output_file \n"
+              "The training file has to contain one word and its corresponding pos tag "
+              "per line, seperated by tabulators or spaces. Each beginning and end of a "
+              "sentence in the data must be marked with an additional <BOS> <BOS> and "
+              "respectively <EOS> <EOS> pair also seperated by tabulators. \n"
+              "Some example files are included in /data. \n";
+      exit(1);
+   }
+   return 0;
 }
 
 #endif
